97df60d02889c2781177480dd34eb6e6aa55c8df
Lucas Gomes <msilva_lucas@sicredi.com.br>
2022-11-01 15:39:38 -0300

    upload archives project

---
+++ src/main/java/br/com/aves/rapina/RapinaApplication.java
@@ -0,0 +1,13 @@
+package br.com.aves.rapina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RapinaApplication.class, args);
+	}
+
+}
