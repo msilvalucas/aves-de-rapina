fe0888ada94fec4ee393b34c1b72bc5b535cfe76
Lucas Pedroso <lucaseduardosilvapedroso@gmail.com>
2022-11-04 16:01:33 -0300

    bug fix: tests config

--- back-end/src/test/java/com/sicredipucrs/AvesDeRapina/controllers/BirdControllerTests.java
+++ back-end/src/test/java/com/sicredipucrs/AvesDeRapina/controllers/BirdControllerTests.java
@@ -5,14 +5,20 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sicredipucrs.AvesDeRapina.dto.BirdDTO;
@@ -37,6 +43,7 @@ public class BirdControllerTests {
     private String existingParam;
     private String nonExistingParam;
     private BirdDTO birdDTO;
+    private List<BirdDTO> list;
 
     @BeforeEach
     void setUp() throws Exception {
@@ -44,11 +51,13 @@ public class BirdControllerTests {
         nonExistingId = 2L;
         existingParam = "Branco";
         nonExistingParam = "Azul";
+
         birdDTO = Factory.createBirdDTO();
+        list = new ArrayList<>(List.of(birdDTO));
 
         //Configuração do comportamento do mock
-        when(service.findBird(any())).thenReturn(new ArrayList<BirdDTO>());
-        when(service.findBird(existingParam)).thenReturn(new ArrayList<BirdDTO>());
+        when(service.findBird(any())).thenReturn(list);
+        when(service.findBird(existingParam)).thenReturn(list);
         when(service.findBird(nonExistingParam)).thenThrow(ResourceNotFoundException.class);
         when(service.insert(any())).thenReturn(birdDTO);
         when(service.update(eq(existingId), any())).thenReturn(birdDTO);
@@ -58,4 +67,13 @@ public class BirdControllerTests {
     }
 
     //TO-DO
+    @Test
+    public void findBirdWithNoParamsShouldReturnAllBirds() throws Exception {
+        
+        ResultActions result = mockMvc
+            .perform(get("/birds/search?param=")
+            .accept(MediaType.APPLICATION_JSON));
+
+            result.andExpect(MockMvcResultMatchers.status().isOk());
+    }
 }
