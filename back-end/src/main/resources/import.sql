c206e67a2c077efc27f45fa0b8adb629e03a1884
Lucas Pedroso <lucaseduardosilvapedroso@gmail.com>
2022-11-08 13:22:23 -0300

    add database seed

---
+++ back-end/src/main/resources/import.sql
@@ -0,0 +1,11 @@
+insert into bird(color, family, gender, habitat, nameen, namelat, namept, size) values ('Black', 'Columbidae', 'M', 'Urban', 'Rock Dove', 'Columba livia', 'Pombo', 0.10);
+insert into bird(color, family, gender, habitat, nameen, namelat, namept, size) values ('White', 'Dromadidae', 'F', 'Urban', 'Mourning Dove', 'Zenaida macroura', 'Pombo-branco', 0.15);
+insert into bird(color, family, gender, habitat, nameen, namelat, namept, size) values ('Green', 'Phasianidae', 'M', 'Jungle', 'Mallard', 'Anas platyrhynchos', 'Pavão', 0.50);
+insert into bird(color, family, gender, habitat, nameen, namelat, namept, size) values ('Blue', 'Anatidae', 'F', 'Jungle', 'Blue-winged Teal', 'Spatula discors', 'Pato', 0.30);
+insert into bird(color, family, gender, habitat, nameen, namelat, namept, size) values ('Brown', 'Anatidae', 'M', 'Jungle', 'Northern Shoveler', 'Spatula clypeata', 'Pato', 0.30);
+
+insert into users(email, login, name, password) values ('isadora@gmail.com', 'isahoelscher', '1234');
+insert into users(email, login, name, password) values ('lucasgomes@gmail.com', 'lulinux21', '5678');
+insert into users(email, login, name, password) values ('dienifer@gmail.com', 'dienikwi', '91011');
+insert into users(email, login, name, password) values ('lucaspedroso@gmail.com', 'lucaseduardopedroso', '121314');
+insert into users(email, login, name, password) values ('michael@gmail.com', 'mdmmora', '151617');
