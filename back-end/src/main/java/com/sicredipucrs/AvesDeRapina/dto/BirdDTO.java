336ec1fe0bd637217cbdbafe5b86e44fdbbbd6de
Lucas Pedroso <lucaseduardosilvapedroso@gmail.com>
2022-11-08 15:43:31 -0300

    add method argument not valid exception handler

--- back-end/src/main/java/com/sicredipucrs/AvesDeRapina/dto/BirdDTO.java
+++ back-end/src/main/java/com/sicredipucrs/AvesDeRapina/dto/BirdDTO.java
@@ -1,5 +1,9 @@
 package com.sicredipucrs.AvesDeRapina.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
 import com.sicredipucrs.AvesDeRapina.entities.Bird;
 
 import lombok.AllArgsConstructor;
@@ -12,14 +16,20 @@ import lombok.NoArgsConstructor;
 public class BirdDTO {
     private Long id;
 
+    @Size(min = 3, max = 50, message = "Name must contain between 3 and 50 characters")
+    @NotBlank(message = "Name is mandatory")
     private String namePT;
     private String nameEN;
     private String nameLAT;
 
+    @Positive(message = "Size cannot be negative")
     private Double size;
     private String gender;
+    @NotBlank(message = "Color is mandatory")
     private String color;
+    @NotBlank(message = "Family is mandatory")
     private String family;
+    @NotBlank(message = "Habitat is mandatory")
     private String habitat;
 
     public BirdDTO(Bird entity){
