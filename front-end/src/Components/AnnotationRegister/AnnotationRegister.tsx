045de91a7ed3a736a9833e5c598b3a4971ae082d
dienifer_kwiatkowski <dienifer_kwiatkowski@sicredi.com.br>
2022-11-17 01:45:11 -0300

    atualiza id do usuario logado na annotation

--- front-end/src/Components/AnnotationRegister/AnnotationRegister.tsx
+++ front-end/src/Components/AnnotationRegister/AnnotationRegister.tsx
@@ -6,6 +6,7 @@ import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import axios from "axios";
 import "../BirdRegister/BirdRegister.css";
+import api from "../../services/api";
 
 interface IBirdFormState {
   id: number;
@@ -43,7 +44,8 @@ const AnnotationRegister = () => {
   const [birdEscolhido, setBirdEscolhido] = useState<IBirdFormState>();
 
   const [idBird, setIdBird] = useState<Number>();
-  const [idUserLogado, setIdUserLogado] = useState<number>(1);
+
+  const [userName, setUserName] = useState(`${localStorage.getItem("userName")}`);
 
   const [birdFormState, setBirdFormState] = useState<IBirdFormState>({
     id: 0,
@@ -58,7 +60,7 @@ const AnnotationRegister = () => {
   });
 
   const [userFormState, setUserFormState] = useState<IUserFormState>({
-    id: idUserLogado,
+    id: 0,
   });
 
   const [formState, setFormState] = useState<IAnnotationFormState>({
@@ -71,6 +73,7 @@ const AnnotationRegister = () => {
 
   useEffect(() => {
     getBirds();
+    alteraIdUserLogado();
   }, []);
 
   useEffect(() => {
@@ -83,6 +86,20 @@ const AnnotationRegister = () => {
       bird: birdEscolhido || birdFormState,
     });
   }, [birdEscolhido]);
+
+  async function alteraIdUserLogado() {
+    try {
+      const response = await api.get(
+        `http://localhost:8080/users/email/${userName}`
+      );
+      setFormState({
+        ...formState,
+        user: response.data || userFormState,
+      });
+    } catch (error) {
+      alert("Desculpe, login inválido...");
+    }
+  }
 
   const getBirds = () => {
     axios
@@ -100,8 +117,6 @@ const AnnotationRegister = () => {
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    //console.log(birdEscolhido);
-    //console.log(formState, "teste");
     alert("Avistamento cadastrado!");
     axios
       .post("http://localhost:8080/annotations", formState, config)
