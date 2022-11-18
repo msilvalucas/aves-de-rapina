365c4b9cfa5ae35b6494d5df4857d79c027daf6e
Lucas Pedroso <lucaseduardosilvapedroso@gmail.com>
2022-11-18 10:44:56 -0300

    factory adjusts

--- back-end/src/test/java/com/sicredipucrs/AvesDeRapina/tests/Factory.java
+++ back-end/src/test/java/com/sicredipucrs/AvesDeRapina/tests/Factory.java
@@ -13,7 +13,7 @@ public class Factory {
         bird.setNamePT("Pato");
         bird.setNameEN("Duck");
         bird.setNameLAT("Anas platyrhynchos");
-        bird.setSize(0.40);
+        bird.setSize(4);
         bird.setGender("M");
         bird.setColor("Branco");
         bird.setFamily("Anatidae");
